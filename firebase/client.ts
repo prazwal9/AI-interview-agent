975771213b32160a62ab505fac690167af0bfcd0
prazwal9 <prazwalthapa9@gmail.com>
2025-05-01 13:20:31 +0530

    implement interview generation page ui

--- firebase/client.ts
+++ firebase/client.ts
@@ -6,14 +6,15 @@ const firebaseConfig = {
   apiKey: "AIzaSyC4hykgzz6FbAoq2EDHPcrFWInP0tX2UzE",
   authDomain: "preppal-4f3d9.firebaseapp.com",
   projectId: "preppal-4f3d9",
-  storageBucket: "preppal-4f3d9.firebasestorage.app",
+  storageBucket: "preppal-4f3d9.appspot.com",
   messagingSenderId: "814371966433",
   appId: "1:814371966433:web:2f73a571c3e3325a0c96a0",
   measurementId: "G-PJPQ1BYXKQ"
 };
 
 
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+//const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -59,6 +59,11 @@ export async function signIn(params: SignInParams) {
 
         await setSessionCookie(idToken);
 
+        return {
+            success: true,
+            message: 'Signed in successfully'
+        };
+
     }catch(e){
         console.log(e);
 
