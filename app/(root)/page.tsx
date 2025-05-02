e2015b96a1e82065436d39009f24450ed022a97b
prazwal9 <prazwalthapa9@gmail.com>
2025-05-02 16:02:46 +0530

    added working interview session

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,8 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import InterviewCard from '@/components/InterviewCard'
-import { getCurrentUser, getInterviewsByUserId, getLatestInterviews } from '@/lib/actions/auth.action'
+import { getCurrentUser } from '@/lib/actions/auth.action'
+import { getInterviewsByUserId, getLatestInterviews } from '@/lib/actions/general.action'
 
 const page = async () => {
   const user = await getCurrentUser();
